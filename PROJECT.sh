f6e66c11f25681117cff737ce2a041eec9139f87
Daniil Pintjuk <dpintjuk@pchnscicloud004.dyndns.cern.ch>
2017-06-14 14:40:41 +0200

    initial

---
+++ PROJECT.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+export OS_USERNAME=dpintjuk
+export OS_TENANT_NAME="Personal dpintjuk"
+export OS_AUTH_URL=https://keystone.cern.ch/main/v3 
+
+# With Keystone you pass the keystone password.
+echo "Please enter your OpenStack Password: "
+read -sr OS_PASSWORD_INPUT
+export OS_PASSWORD=$OS_PASSWORD_INPUT
