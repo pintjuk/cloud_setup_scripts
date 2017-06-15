c6981c18641d333bcb81aeeadbf097c6155dab75
Daniil Pintjuk <dpintjuk@pchnscicloud004.dyndns.cern.ch>
2017-06-15 10:39:18 +0200

    minor fixes to setup script

--- setup_test_enviroment.sh
+++ setup_test_enviroment.sh
@@ -1,26 +1,38 @@
 #! /bin/bash
 echo "yes | sudo yum install git"
 yes | sudo yum install git
+echo ""
 echo "git clone https://github.com/pintjuk/cloud_setup_scripts.git"
 git clone https://github.com/pintjuk/cloud_setup_scripts.git
 cd cloud_setup_scripts
-sudo su
+echo ""
 echo "yum install gcc-c++.x86_64"
-yum install gcc-c++.x86_64
+yes | yum install gcc-c++.x86_64
+echo ""
 echo "yum install  wget"
-yum install  wget
+yes | yum install  wget
+echo ""
 echo "yum install  redhat-lsb-core"
-yum install  redhat-lsb-core
+yes | yum install  redhat-lsb-core
+echo ""
 echo "yum install python"
-yum install python
+yes | yum install python
+echo ""
 echo "yum install python-pip"
-yum install python-pip
+yes | yum install python-pip
+echo ""
+echo "updgrade pip to lates version"
+pip install --upgrade pip
+echo ""
 echo "yum install make"
-yum install make
+yes | yum install make
+echo ""
 echo "export LC_ALL='en_US'"
 export LC_ALL="en_US"
+echo ""
 echo "pip install stomp.py"
 pip install stomp.py
+echo ""
 echo "pip install SOAPpy"
 pip install SOAPpy
 #cernvm
@@ -28,7 +40,9 @@ pip install SOAPpy
 # yum install cvmfs cvmfs-config-default
 # cvmfs_config setup
 cd cloud-benchmark-suite
+echo ""
 echo "building benchmarks"
 make all
+echo ""
 echo "runing benchmarks"
 ../testrun
