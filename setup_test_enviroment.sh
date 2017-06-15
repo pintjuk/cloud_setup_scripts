c577afe9e6be99d942281caccc9f994c56509362
Daniil Pintjuk <dpintjuk@pchnscicloud004.dyndns.cern.ch>
2017-06-15 10:14:18 +0200

    changed to http get from github to ovoid authentication

--- setup_test_enviroment.sh
+++ setup_test_enviroment.sh
@@ -1,13 +1,8 @@
 #! /bin/bash
 yes | sudo yum install git
-git clone ssh://git@gitlab.cern.ch:7999/dpintjuk/HNSciCloudTesting.git
-cd HNsciCloudTesting
-
-echo '[gitlab.cern.ch]:7999,[188.184.66.88]:7999 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBswtRsTLbTluK4lN2gk71Wp020NxNZcihjhUfUoJ7+hrHamzX5wjZBjwEBtgyzISYrstd1giRrP5qfqIf+dUyY=' >>  ~/.ssh/known_hosts 
-git clone ssh://git@gitlab.cern.ch:7999/dpintjuk/HNSciCloudTesting.git
-
+git clone https://github.com/pintjuk/cloud_setup_scripts.git
+cd cloud_setup_scripts
 sudo su
-
 yum install gcc-c++.x86_64
 yum install  wget
 yum install  redhat-lsb-core
@@ -15,17 +10,12 @@ yum install python
 yum install python-pip
 yum install make
 export LC_ALL="en_US"
-
 pip install stomp.py
 pip install SOAPpy
-
-
 #cernvm
 # already installed and proerly configured in the SL6 image
-
 # yum install cvmfs cvmfs-config-default
 # cvmfs_config setup
-
-
 cd cloud-benchmark-suite
 make all
+../testrun
